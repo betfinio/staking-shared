8de2aa7a0c2999daebc36c90996c85bd039afa29
Monster <bf.monster@proton.me>
2025-01-16 16:20:15 +0100

    feat: added pools distributions

--- gelato/web3-functions/distribute/index.ts
+++ gelato/web3-functions/distribute/index.ts
@@ -2,75 +2,41 @@ import {
   Web3Function,
   Web3FunctionContext,
 } from "@gelatonetwork/web3-functions-sdk";
-import {
-  Address,
-  createPublicClient,
-  encodeFunctionData,
-  http,
-  parseAbi,
-} from "viem";
-import { polygon, polygonAmoy } from "viem/chains";
+import { Address, encodeFunctionData, parseAbi } from "viem";
+import { request } from "graphql-request";
 
-const abi = parseAbi([
-  "function calculateProfit(uint256 offset, uint256 count) external",
-  "function currentPool() external view returns(address)",
-  "function distributeProfit() external",
-  "function getActivePoolCount() external view returns(uint256)",
-]);
+const query = (staking: Address, last: number) => `{
+  pools(where: {staking: "${staking}", lastDistributed_lt: "${last}"}, first: 10) {
+    pool
+  }
+}`;
+const abi = parseAbi(["function distributeProfit() external"]);
 
 Web3Function.onRun(async (context: Web3FunctionContext) => {
-  const { multiChainProvider, userArgs, gelatoArgs, secrets } = context;
-  const provider = multiChainProvider.default();
-  const url = provider.connection.url;
-  // initialize client
-  const client = createPublicClient({
-    chain: gelatoArgs.chainId === 80002 ? polygonAmoy : polygon,
-    transport: http(url),
-  });
-
-  // eslint-disable-next-line no-async-promise-executor
-  await new Promise(async (resolve) =>
-    setTimeout(resolve, Number(await secrets.get("DELAY"))),
-  );
-
-  // get luro address
+  const { userArgs, secrets } = context;
+  // get staking address
   const stakingAddress: Address = userArgs.staking as Address;
 
-  // get previous round
-  const currentPool: Address = await client.readContract({
-    address: stakingAddress,
-    abi: abi,
-    functionName: "currentPool",
-    args: [],
-  });
-  const poolsCount = await client.readContract({
-    address: stakingAddress,
-    abi: abi,
-    functionName: "getActivePoolCount",
-    args: [],
-  });
-  // split the pools into chunks of 50
-  const pools: { to: Address; data: Address }[] = [];
-  for (let i = BigInt(0); i < poolsCount; i += BigInt(50)) {
-    pools.push({
-      to: stakingAddress,
-      data: encodeFunctionData({
-        abi: abi,
-        functionName: "calculateProfit",
-        args: [i, BigInt(50)],
-      }),
-    });
-  }
-  const currentPoolDistribution = {
-    to: currentPool,
+  // get graph url
+  const graphUrl = (await secrets.get("STAKING_URL"))!;
+
+  const last = Math.floor(Date.now() / 1000) - 60 * 60 * 24; // fetch pools that was not distributed in last 24 hours
+
+  const data = await request<{ pools: { pool: Address }[] }>(
+    graphUrl,
+    query(stakingAddress, last),
+  );
+  const pools = data.pools.map((pool) => ({
+    to: pool.pool,
     data: encodeFunctionData({
-      abi: abi,
+      abi,
       functionName: "distributeProfit",
       args: [],
     }),
-  };
+  }));
+
   return {
     canExec: true,
-    callData: [...pools, currentPoolDistribution],
+    callData: pools,
   };
 });
