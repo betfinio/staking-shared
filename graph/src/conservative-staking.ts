7d9ab63bb7f404f41db3b411cb3616b72c7cce7f
BF san <bf-san@proton.me>
2025-07-22 15:19:45 +0200

    feat: enhance staking logic with dynamic and conservative withdraw handling

--- graph/src/conservative-staking.ts
+++ graph/src/conservative-staking.ts
@@ -1,4 +1,3 @@
-// biome-ignore lint/style/useImportType: not supported
 import {
 	Claimed as ClaimedEvent,
 	ConservativeStaking,
@@ -6,10 +5,9 @@ import {
 	Staked as StakedEvent,
 	Withdraw as WithdrawEvent,
 } from "../generated/ConservativeStaking/ConservativeStaking";
-import { Claim, Pool, Stake, Withdraw } from "../generated/schema";
+import { Claim, Pool, Stake } from "../generated/schema";
 import { ConservativeStakingPool } from "../generated/ConservativeStaking/ConservativeStakingPool";
-// biome-ignore lint/suspicious/noShadowRestrictedNames: shadowing
-import { BigInt, Bytes, DataSourceContext } from "@graphprotocol/graph-ts";
+import { Address, BigInt, Bytes, DataSourceContext } from "@graphprotocol/graph-ts";
 import { ConservativeStakingPool as PoolTemplate } from "../generated/templates";
 
 export function handlePoolOpened(event: PoolOpenedEvent): void {
@@ -20,6 +18,7 @@ export function handlePoolOpened(event: PoolOpenedEvent): void {
 	entity.blockTimestamp = event.block.timestamp;
 	entity.transactionHash = event.transaction.hash;
 	entity.lastDistributed = event.block.timestamp;
+	entity.stakes = [];
 	entity.save();
 
 	const context = new DataSourceContext();
@@ -62,12 +61,16 @@ export function handleStaked(event: StakedEvent): void {
 	const entity = new Stake(
 		event.transaction.hash.concatI32(event.logIndex.toI32()),
 	);
+
 	entity.staker = event.params.staker;
 	entity.amount = event.params.amount;
 
 	entity.blockNumber = event.block.number;
 	entity.blockTimestamp = event.block.timestamp;
 	entity.transactionHash = event.transaction.hash;
+	entity.unstaked = false;
+	entity.unstakedAt = BigInt.fromI32(0);
+
 	const lastPoolIndex = ConservativeStaking.bind(
 		event.address,
 	).getStakedPoolsCount(event.params.staker);
@@ -75,6 +78,7 @@ export function handleStaked(event: StakedEvent): void {
 		event.params.staker,
 		lastPoolIndex.minus(BigInt.fromI32(1)),
 	);
+
 	entity.pool = pool;
 	entity.unlock = ConservativeStakingPool.bind(pool)
 		.getStake(event.params.staker)
@@ -84,18 +88,70 @@ export function handleStaked(event: StakedEvent): void {
 	);
 	entity.address = event.address;
 	entity.save();
+
+	const poolEntity = Pool.load(pool);
+	if (poolEntity) {
+		poolEntity.stakes = poolEntity.stakes.concat([entity.id]);
+		poolEntity.save();
+	}
+
+
+
+
 }
 
-export function handleWithdraw(event: WithdrawEvent): void {
-	const entity = new Withdraw(
-		event.transaction.hash.concatI32(event.logIndex.toI32()),
-	);
-	entity.staker = event.params.staker;
-	entity.amount = event.params.amount;
+export function handleConservativeWithdraw(event: WithdrawEvent): void {
 
-	entity.blockNumber = event.block.number;
-	entity.blockTimestamp = event.block.timestamp;
-	entity.transactionHash = event.transaction.hash;
 
-	entity.save();
+	const receipt = event.receipt;
+
+	const transferSignature = Bytes.fromHexString("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"); // signature of ERC20 transfer event
+	let pool: Address | null = null;
+	if (receipt) {
+
+		const txLogs = receipt.logs;
+
+
+
+		for (let i = 0; i < txLogs.length; i++) {
+
+			const txLog = txLogs[i];
+
+			if (txLog.topics[0].equals(transferSignature)) {
+				const fromTopic = txLog.topics[1];
+				const from = '0x' + fromTopic.toHexString().slice(-40); // Get the last 40 characters(Address)
+				pool = Address.fromString(from);
+				break;
+			}
+		}
+
+	}
+
+	if (pool) {
+
+		const staker = event.params.staker;
+		handleWithdraw(pool, event.block.timestamp, staker, event.transaction.hash);
+	}
+}
+
+function handleWithdraw(pool: Address, blockTimestamp: BigInt, staker: Address, transactionHash: Bytes): void {
+
+
+
+	const poolEntity = Pool.load(pool);
+	if (poolEntity) {
+
+		for (let i = 0; i < poolEntity.stakes.length; i++) {
+			const stake = poolEntity.stakes[i];
+			const stakeEntity = Stake.load(stake);
+			if (stakeEntity && stakeEntity.staker.equals(staker) && stakeEntity.unstaked == false) {
+				stakeEntity.unstaked = true;
+				stakeEntity.unstakedAt = blockTimestamp;
+				stakeEntity.unstakeTransactionHash = transactionHash;
+				stakeEntity.save();
+			}
+		}
+	}
+
+
 }
--- graph/src/dynamic-staking.ts
+++ graph/src/dynamic-staking.ts
@@ -3,11 +3,12 @@ import {
 	DynamicStaking,
 	PoolOpened as PoolOpenedEvent,
 	Staked as StakedEvent,
+	Withdraw as WithdrawEvent,
 } from "../generated/DynamicStaking/DynamicStaking";
 import { Pool, Stake } from "../generated/schema";
 import { DynamicStakingPool } from "../generated/DynamicStaking/DynamicStakingPool";
 import { DynamicStakingPool as PoolTemplate } from "../generated/templates";
-import { DataSourceContext } from "@graphprotocol/graph-ts";
+import { Address, BigInt, Bytes, DataSourceContext } from "@graphprotocol/graph-ts";
 
 export function handlePoolOpened(event: PoolOpenedEvent): void {
 	const entity = new Pool(event.params.pool);
@@ -17,6 +18,7 @@ export function handlePoolOpened(event: PoolOpenedEvent): void {
 	entity.blockTimestamp = event.block.timestamp;
 	entity.lastDistributed = event.block.timestamp;
 	entity.transactionHash = event.transaction.hash;
+	entity.stakes = [];
 	entity.save();
 
 	const context = new DataSourceContext();
@@ -33,7 +35,8 @@ export function handleStaked(event: StakedEvent): void {
 	entity.amount = event.params.amount;
 
 	entity.address = event.address;
-
+	entity.unstaked = false;
+	entity.unstakedAt = BigInt.fromI32(0);
 	entity.blockNumber = event.block.number;
 	entity.blockTimestamp = event.block.timestamp;
 	entity.transactionHash = event.transaction.hash;
@@ -43,4 +46,43 @@ export function handleStaked(event: StakedEvent): void {
 	entity.reward = DynamicStakingPool.bind(pool).getClaimed(event.params.staker);
 
 	entity.save();
+
+	const poolEntity = Pool.load(pool);
+	if (poolEntity) {
+		poolEntity.stakes = poolEntity.stakes.concat([entity.id]);
+		poolEntity.save();
+	}
+
+
+	
+}
+
+
+export function handleDynamicWithdraw(event: WithdrawEvent): void {
+	const pool = event.params.pool;
+	const stakersCount = DynamicStakingPool.bind(event.params.pool).getStakersCount();
+	for (let i = 0; i < stakersCount.toI32(); i++) {
+		const staker = DynamicStakingPool.bind(event.params.pool).stakers(BigInt.fromI32(i));
+		handleWithdraw(pool, event.block.timestamp, staker, event.transaction.hash);
+	}
+
+
+}
+
+function handleWithdraw(pool: Address, blockTimestamp: BigInt, staker: Address, transactionHash: Bytes): void {
+
+	const poolEntity = Pool.load(pool);
+	if (poolEntity) {
+		for (let i = 0; i < poolEntity.stakes.length; i++) {
+			const stake = poolEntity.stakes[i];
+			const stakeEntity = Stake.load(stake);
+			if (stakeEntity && stakeEntity.staker.equals(staker) && stakeEntity.unstaked == false) {
+				stakeEntity.unstaked = true;
+				stakeEntity.unstakedAt = blockTimestamp;
+				stakeEntity.unstakeTransactionHash = transactionHash;
+				stakeEntity.save();
+			}
+		}
+	}
+
 }
